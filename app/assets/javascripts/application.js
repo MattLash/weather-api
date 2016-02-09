467891b5e5635c2ffd86c04b9c34f4fa15bde53d
Matt Lashinsky <malashinsky@gmail.com>
2016-02-09 23:15:22 +0000

    final updates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,7 +25,8 @@ $(document).ready(function(){
     var userAndToken = username + ":" + token;
     var encodedToken = window.btoa(userAndToken);
     var encodedRequest = "Basic " + encodedToken;
-    
+    //create DOM element for Plot
+    var weatherPlot = document.getElementById('weather_plot');
     //============== FUNCTION DEFINITIONS =============== //
     function getStations(deployment){
         $.ajax({
@@ -99,7 +100,7 @@ $(document).ready(function(){
         }).fail(function(){
             console.log("The first AJAX is not working!");
         });
-        return [tempArray,timeArray];
+        return [tempArray,timeArray,deployment];
     }
     function timeConversion(millisec) {
         // Create a new JavaScript Date object based on the timestamp
@@ -118,11 +119,12 @@ $(document).ready(function(){
     }
     function plotData(weatherData){
         //plotly takes 3 arugments: the DOM element plot to, plot data & attributes "traces", layout
-        var weatherPlot = document.getElementById('weather_plot');
         
         var data = [{
+            
             x: weatherData[1],
-            y: weatherData[0]
+            y: weatherData[0],
+            name: capitalizeFirstLetter(weatherData[2])
         }];
         var layout = {
             autoscale: true,
@@ -139,41 +141,88 @@ $(document).ready(function(){
                 hovermode: 'closest'
         };
     	Plotly.plot( weatherPlot, data, layout);
+    	setTimeout(function(){document.querySelector('[data-title="Autoscale"]').click();},400);
+    	setTimeout(function(){document.querySelector('[data-title="Autoscale"]').click();},1000);
     }
-    function updatePlotAxes(){ 
-        document.querySelector('[data-title="Autoscale"]').click();
-        console.log("Updating the plot axes now!");
-        
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
     }
-    function updatePlots(){
-        Plotly.deleteTraces(document.getElementById('weather_plot'), [0,1]);
-        var updatedSomervilleData = getWeatherData('somerville','6ceceb681a5c');
-        var updatedDallasData = getWeatherData('dallas','6ceceb6634e8');
-        
-        plotData(updatedSomervilleData);
-        plotData(updatedDallasData);
-        setTimeout(updatePlotAxes,400);
-        console.log('updating plot data and axes');
-    }
-    
+   
     //============== FUNCTION CALLS =============//
-    //-- call initial stations and weather data for Somerville & Dallas
+    //-- Populate table with station data for Somerville & Dallas
     getStations('somerville');
     getStations('dallas');
     
+    //cache frequent variables
+    var $SB =  $('#somerville-button');
+    var $DB = $('#dallas-button');
+    var $autoscale = document.querySelector('[data-title="Autoscale"]');
     
+    //-- User Buttons
+    $SB.click(function(){
+        
+        if( $SB.hasClass('active') && $DB.hasClass('active') ){
+            Plotly.deleteTraces(weatherPlot, [-2,-1]);
+            var dallasWeatherData = getWeatherData('dallas', '6ceceb6634e8');
+            plotData(dallasWeatherData);
+            $SB.removeClass('active');
+        }else if($SB.hasClass('active')){
+            Plotly.deleteTraces(weatherPlot, 0);
+            $SB.removeClass('active');
+        }else{
+            var somervilleWeatherData = getWeatherData('somerville','6ceceb681a5c');
+            plotData(somervilleWeatherData);
+            $SB.addClass('active');
+        }
+    });
     
-    // var somervilleStationId = getStationId('somerville');
-    
-    var somervilleWeatherData = getWeatherData('somerville','6ceceb681a5c');
-    
-    var dallasWeatherData = getWeatherData('dallas', '6ceceb6634e8');
-    //-- plot initial somerville data
-    plotData(somervilleWeatherData);
-    plotData(dallasWeatherData);
+    $DB.click(function(){
+        if( $SB.hasClass('active') && $DB.hasClass('active') ){
+            Plotly.deleteTraces(weatherPlot, [-2,-1]);
+           var somervilleWeatherData = getWeatherData('somerville','6ceceb681a5c');
+            plotData(somervilleWeatherData);
+            $DB.removeClass('active');
+        }else if( $DB.hasClass('active')){
+            Plotly.deleteTraces(weatherPlot, 0);
+            $DB.removeClass('active');
+        }else{
+            var dallasWeatherData = getWeatherData('dallas', '6ceceb6634e8');
+            plotData(dallasWeatherData);
+            $DB.addClass('active');
+        }
+    });
+     
+    // function to refresh active plots
+    function updatePlots(){
+        
+        if( $SB.hasClass('active') && $DB.hasClass('active') ){
+            //if both are active, then refresh both
+            Plotly.deleteTraces(weatherPlot, [-2,-1]);
+            var updatedSomervilleData = getWeatherData('somerville','6ceceb681a5c');
+            var updatedDallasData = getWeatherData('dallas','6ceceb6634e8');
+            plotData(updatedSomervilleData);
+            plotData(updatedDallasData);
+            setTimeout(function(){$autoscale.click();},400);
+        }else if( $DB.hasClass('active')){
+            //if only dallas is active, refresh dallas
+            Plotly.deleteTraces(weatherPlot, 0);
+            var updatedDallasData = getWeatherData('dallas','6ceceb6634e8');
+            setTimeout(function(){$autoscale.click();},400);
+            plotData(updatedDallasData);
+        }else if($SB.hasClass('active')){
+            //refresh somerville if active
+            Plotly.deleteTraces(weatherPlot, 0);
+            var updatedSomervilleData = getWeatherData('somerville','6ceceb681a5c');
+            plotData(updatedSomervilleData);
+            setTimeout(function(){$autoscale.click();},400);
+        }else{
+            
+        }
+      
+    }
     
     //-- update axes to fit initial dataset
-    setTimeout(updatePlotAxes,400);
+    // setTimeout(updatePlotAxes,400);
     //-- update with fresh data and update chart every 30 seconds
     window.setInterval(updatePlots, 30000);
     
